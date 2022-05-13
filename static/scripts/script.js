39e6b1b56fd3b898afcb5a35dc5d724664736bbf
Ayyoub kalache <kalache.ayyoub@umc.edu.dz>
2022-05-13 06:39:23 +0100

    Clean

--- static/scripts/script.js
+++
@@ -1,9 +0,0 @@
-alert('hiiiiiiiiiiiiiii');
-document.ready(
-    function(){ 
-        console.log('wwwwwww');
-        document.getElementById("#exampleModalCenter").modal('show') ;}
-)
-// ${document}.ready( function(){ 
-//     console.log('wwwwwww');
-//     ${"#exampleModalCenter"}.modal('show') ;}); 
