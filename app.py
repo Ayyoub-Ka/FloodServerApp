9e94e7c6b0b2e71e314ab88d44709be91c50703e
Ayyoub kalache <kalache.ayyoub@umc.edu.dz>
2022-05-13 01:29:30 +0100

    vers viga

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, url_for ,Response
-from flask import request
+from flask import Flask, render_template, redirect, url_for ,Response,make_response
+from flask import request,jsonify
 from requests import post
 import requests
 import pandas as pd
@@ -9,12 +9,12 @@ import vincent
 import random
 import io
 import json
-
+import mpld3
+from mpld3 import plugins
+from matplotlib import pyplot as plt
+import base64
 
 app = Flask(__name__)
-
-
-
 
 api_root = "http://environment.data.gov.uk/flood-monitoring";
 
@@ -31,13 +31,10 @@ def home():
 def getSelectedStation():
     output = request.get_json()
     print(output)
-    #getPLot(output)
+    code=getHtmlPlot(output)
     #return redirect(url_for('/plot.png', result_id=output))
-    res = make_response(jsonify({"message": "OK"}), 200)
-    return  redirect(url_for('/plot.png', result_id=output))
-
-
-
+    #return  make_response(jsonify(plot_png(1)), 200)
+    return  make_response(plot_png64(), 200)
 
 def getDataX():
     # if (stations or stations!=[]) :
@@ -56,6 +53,16 @@ def getLocations(stations):
     return filtered_stations[['lat', 'long', 'notation']].to_dict("records")
 
 def getPlot(id):
+    df ,_= preparePlot(id)
+    line = vincent.Line(df)
+    line.axis_titles(x='dateTime', y='value')
+    line.legend(title='Categories')
+    return line.to_json()
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+def preparePlot(id):
     if id=='':
         return 
 
@@ -82,20 +89,7 @@ def getPlot(id):
     #df['measure']=df['measure'].map(lambda m: m[m.rfind('/'):] ).map(lambda m: m[m.find('-')+1:])
     df['measure']=df['measure'].map(lambda m:sm.loc[sm['@id'] == m, ['parameterName','qualifier']].iloc[0].str.cat(sep='-'))
     x=df.pivot(index='dateTime', columns='measure', values='value').reset_index()
-    line = vincent.Line(df)
-    line.axis_titles(x='dateTime', y='value')
-    line.legend(title='Categories')
-    return line.to_json()
-
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-
-@app.route('/plot.png')
-def plot_png(result_id):
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
+    return df, x
 
 def create_figure():
     fig = Figure()
@@ -105,8 +99,30 @@ def create_figure():
     axis.plot(xs, ys)
     return fig
 
+def plot_png(result_id):
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def plot_png64():
+    my_stringIObytes = io.BytesIO()
+    create_figure().savefig(my_stringIObytes, format='jpg')
+    my_stringIObytes.seek(0)
+    my_base64_jpgData = base64.b64encode(my_stringIObytes.read())
+    return my_base64_jpgData
+
+def getHtmlPlot(id):
+    _,x =preparePlot(id)
+    fig= plt.figure()
+    #ax = fig.add_subplot(1,1,1)
+    ax=x.plot(x='dateTime', fontsize=15)
+    # Additional customizations
+    ax.set_xlabel('Date')
+    ax.legend(fontsize=14)
+    return mpld3.fig_to_html(fig)
+
+
+
